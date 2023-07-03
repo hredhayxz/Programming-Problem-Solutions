023fc361bbc27e29b8a9d42ec91c5b086c038774
Md Alhaz Mondal Hredhay <60058949+hredhayxz@users.noreply.github.com>
2023-07-03 19:54:11 +0600

    This file for practice

--- C++/Practice.cpp
+++ C++/Practice.cpp
@@ -1,15 +1,30 @@
 #include<bits/stdc++.h>
+
 using namespace std;
-int main()
-{
-    int n=12;
-    //cout<<(n&~(n-1));
-    int position = 0;
-    while (n > 0)
-    {
-        n = n >> 1;
-        position++;
+
+int main() {
+  int nums[] = {0, 4, 3, 2, 7, 8, 2, 3, 1};
+  int n = sizeof(nums) / sizeof(nums[0]);
+
+  // Create an unordered_map to store the integers and their frequencies.
+  unordered_map<int, int> my_map;
+
+  for (int i = 0; i < n; i++) {
+    my_map[nums[i]]++;
+  }
+
+  // Iterate over the unordered_map and find all the integers with frequency 2.
+  vector<int> duplicates;
+  for (auto& it:my_map) {
+    if (it.second == 2) {
+      duplicates.push_back(it.first);
     }
-    cout<<position;
-    return 0;
+  }
+
+  // Print the duplicates.
+  for (int i = 0; i < duplicates.size(); i++) {
+    cout << duplicates[i] << " ";
+  }
+
+  return 0;
 }
