53afa68b3494c6be2e11089abe64d40faa1707a8
Md Alhaz Mondal Hredhay <60058949+hredhayxz@users.noreply.github.com>
2023-07-03 12:44:39 +0600

    1207. Unique Number of Occurrences
    https://leetcode.com/problems/unique-number-of-occurrences/

---
+++ C++/HashTable/1207. Unique Number of Occurrences.cpp
@@ -0,0 +1,22 @@
+/// Ques link: https://leetcode.com/problems/unique-number-of-occurrences/
+
+class Solution
+{
+public:
+    bool uniqueOccurrences(vector<int>& arr)
+    {
+        unordered_map<int,int> mp;
+        unordered_set<int> st;
+        for(auto it:arr)
+        {
+            mp[it]++;
+        }
+        for(auto it:mp)
+        {
+            st.insert(it.second);
+        }
+        return st.size() == mp.size();
+
+    }
+};
+
