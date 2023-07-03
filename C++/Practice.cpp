c8ab3ab1f7409e514faa75b0de80aaee52978bca
Md Alhaz Mondal Hredhay <60058949+hredhayxz@users.noreply.github.com>
2023-07-03 20:16:42 +0600

    1

--- C++/Practice.cpp
+++ C++/Practice.cpp
@@ -1,30 +1,31 @@
-#include<bits/stdc++.h>
-
+#include <bits/stdc++.h>
 using namespace std;
+vector<int> findDuplicates(vector<int>& nums)
+{
+    unordered_map<int, int> mp;
 
-int main() {
-  int nums[] = {0, 4, 3, 2, 7, 8, 2, 3, 1};
-  int n = sizeof(nums) / sizeof(nums[0]);
-
-  // Create an unordered_map to store the integers and their frequencies.
-  unordered_map<int, int> my_map;
-
-  for (int i = 0; i < n; i++) {
-    my_map[nums[i]]++;
-  }
-
-  // Iterate over the unordered_map and find all the integers with frequency 2.
-  vector<int> duplicates;
-  for (auto& it:my_map) {
-    if (it.second == 2) {
-      duplicates.push_back(it.first);
+    for (int i=0; i<nums.size(); i++)
+    {
+        mp[nums[i]]++;
     }
-  }
+    vector<int> duplicates;
+    for (auto& it:mp)
+    {
+        if (it.second==2)
+        {
+            duplicates.push_back(it.first);
+        }
+    }
 
-  // Print the duplicates.
-  for (int i = 0; i < duplicates.size(); i++) {
-    cout << duplicates[i] << " ";
-  }
+    return duplicates;
+}
 
-  return 0;
+int main()
+{
+    vector<int> numRay = { 0, 4, 3, 2, 7, 8, 2, 3, 1 };
+
+    vector<int> ans = findDuplicates(numRay);
+    for (int i : ans)
+        cout << i << ' ' << endl;
+    return 0;
 }
