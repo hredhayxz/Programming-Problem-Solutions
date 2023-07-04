9ca667372e58407d3a1f7874fd1958676b333c00
Md Alhaz Mondal Hredhay <60058949+hredhayxz@users.noreply.github.com>
2023-07-04 12:55:53 +0600

    two-pointer approach
    https://leetcode.com/problems/two-sum/

---
+++ C++/HashTable/Two sum.cpp
@@ -0,0 +1,31 @@
+///Ques link: https://leetcode.com/problems/two-sum/
+
+
+///two-pointer approach
+
+
+class Solution
+{
+public:
+    vector<int> twoSum(vector<int>& nums, int target)
+    {
+        unordered_map<int, int> numToIndex;
+        vector<int> result;
+
+        for (int i = 0; i < nums.size(); ++i)
+        {
+            int complement = target - nums[i];
+
+            if (numToIndex.find(complement) != numToIndex.end())
+            {
+                result.push_back(numToIndex[complement]);
+                result.push_back(i);
+                break;
+            }
+
+            numToIndex[nums[i]] = i;
+        }
+
+        return result;
+    }
+};
