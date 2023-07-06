07a522f6db97819a247759c91c1c531a6dc379ca
Md Alhaz Mondal Hredhay <60058949+hredhayxz@users.noreply.github.com>
2023-07-06 16:10:25 +0600

    https://leetcode.com/problems/sqrtx/
    https://leetcode.com/problems/sqrtx/

---
+++ C++/Searching/69. Sqrt(x).cpp
@@ -0,0 +1,39 @@
+/// Ques link: https://leetcode.com/problems/sqrtx/
+
+
+class Solution
+{
+public:
+
+    int binarySearch(int n)
+    {
+        long long int left=0,right=n,mid,ans=INT_MIN,mul;
+        while(left<=right)
+        {
+            mid=left+((right-left)/2);
+            mul=mid*mid;
+            if(mul==n)
+            {
+                return mid;
+            }
+            else
+            {
+                if(mul<n)
+                {
+                    ans=mid;
+                    left=mid+1;
+                }
+                else
+                {
+                    right=mid-1;
+                }
+            }
+        }
+        return ans;
+    }
+
+    int mySqrt(int x)
+    {
+        return binarySearch(x);
+    }
+};
