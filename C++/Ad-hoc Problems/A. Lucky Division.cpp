67596c2320cabf03d9f68cff931c7998e21ad92f
Md Alhaz Mondal Hredhay <60058949+hredhayxz@users.noreply.github.com>
2023-07-08 00:59:04 +0600

    https://codeforces.com/problemset/problem/122/A
    https://codeforces.com/problemset/problem/122/A

--- C++/Ad-hoc Problems/A. Lucky Division.cpp
+++ C++/Ad-hoc Problems/A. Lucky Division.cpp
@@ -10,7 +10,7 @@ int main()
     int arr[12]={4,7,44,47,74,77,444,447,477,744,774,777};
     for(int i=0;i<12;i++)
     {
-        if(n==arr[i]||n%arr[i]==0)
+        if(n%arr[i]==0)
         {
             cout<<"YES"<<endl;
             return 0;
