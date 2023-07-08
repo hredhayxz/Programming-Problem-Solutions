0a29fc2b5c8706035db23ef8065caa64c5ce02b8
Md Alhaz Mondal Hredhay <60058949+hredhayxz@users.noreply.github.com>
2023-07-09 00:35:25 +0600

    https://www.codingninjas.com/studio/problems/bubble-sort_980524
    https://www.codingninjas.com/studio/problems/bubble-sort_980524

--- C++/Sorting/Bubble Sort.cpp
+++ C++/Sorting/Bubble Sort.cpp
@@ -6,12 +6,18 @@ void bubbleSort(vector<int>& arr, int n)
 {
     for(int i=0; i<n; i++)
     {
+        bool isSwapped=false;
         for(int j=0; j<n-1; j++)
         {
             if(arr[j]>arr[j+1])
             {
                 swap(arr[j+1],arr[j]);
+                isSwapped=true;
             }
+        }
+        if(isSwapped==false)
+        {
+            break;
         }
     }
 }
