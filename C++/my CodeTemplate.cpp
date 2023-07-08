f1aa8d560ff9e6addb2d8375e43b1d6e1ffcbbfd
Md Alhaz Mondal Hredhay <60058949+hredhayxz@users.noreply.github.com>
2023-07-08 10:45:50 +0600

    coding template

---
+++ C++/my CodeTemplate.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define pb push_back
+#define sz size()
+#define endl "\n"
+#define ll long long int
+#define llu unsigned long long int
+#define ld long double
+#define az str.begin(),str.end()
+#define Ta_Ta return 0
+#define pi acos(-1)
+int cs=1;
+void solve(){
+    //string str;
+    //cin>>str;
+    //cin>>N;
+
+    //cout<<"Case #"<<cs++<<": ";
+    //cout<<"Case "<<cs++<<": ";
+    //cout<< <<endl;
+}
+int main(){
+    int T=1;
+    //cin>>T;
+    while(T--) solve();
+    Ta_Ta;
+}
