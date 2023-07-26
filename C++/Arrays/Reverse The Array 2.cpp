76f1b3468932f6aa480afb6e13b5ac30d363ce7a
Md Alhaz Mondal Hredhay <60058949+hredhayxz@users.noreply.github.com>
2023-07-26 16:49:15 +0600

    https://www.codingninjas.com/studio/problems/reverse-the-array_1262298
    https://www.codingninjas.com/studio/problems/reverse-the-array_1262298

---
+++ C++/Arrays/Reverse The Array 2.cpp
@@ -0,0 +1,15 @@
+/// Ques link: https://www.codingninjas.com/studio/problems/reverse-the-array_1262298
+
+
+#include <bits/stdc++.h>
+void reverseArray(vector<int> &arr, int m)
+{
+    int start=m+1;
+    int end=arr.size()-1;
+    while(start<=end)
+    {
+        swap(arr[start],arr[end]);
+        start++;
+        end--;
+    }
+}
