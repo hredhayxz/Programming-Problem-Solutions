6425bf81c7ceddddbdbd14078468f8cee3a99171
Md Alhaz Mondal Hredhay <60058949+hredhayxz@users.noreply.github.com>
2023-08-05 21:47:06 +0600

    https://leetcode.com/problems/remove-all-occurrences-of-a-substring/
    https://leetcode.com/problems/remove-all-occurrences-of-a-substring/

---
+++ C++/Arrays/1910. Remove All Occurrences of a Substring.cpp
@@ -0,0 +1,16 @@
+/// Ques link: https://leetcode.com/problems/remove-all-occurrences-of-a-substring/
+
+
+
+class Solution
+{
+public:
+    string removeOccurrences(string s, string part)
+    {
+        while(s.size()!=0&&s.find(part)<s.size())
+        {
+            s.erase(s.find(part),part.size());
+        }
+        return s;
+    }
+};
